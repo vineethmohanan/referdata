b4dd1e70fd033e4aa5537091ccda53cde509435a
vineethmohanan <mail2vineethm@gmail.com>
2016-11-14 16:26:11 +0530

    Power.py
    to get the power -input base and exponent value

---
+++ Power.py
@@ -0,0 +1,8 @@
+def power(b,e):  # Add your parameters here!
+    b=int(b)
+    e=int(e)
+    result = b**e
+    print "%d to the power of %d is %d." % (b, e, result)
+    
+
+power(raw_input("Input base value"),raw_input("input exponent value"))  # Add your arguments here!
