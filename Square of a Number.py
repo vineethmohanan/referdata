cb5752eb2bc89b716d22bb5671b4fd3a79327f1f
vineethmohanan <mail2vineethm@gmail.com>
2016-11-14 16:14:16 +0530

    Square of Number .py
    To get the square of any numbers.

---
+++ Square of a Number.py
@@ -0,0 +1,9 @@
+
+def square(n):
+    n=int(n)
+    """Returns the square of a number."""
+    squared = n**2
+    print "%d squared is %d." % (n, squared)
+    return squared
+  
+square(raw_input("input \n"))
